41b35d2787a76aae8a83f8df80a0b96f0ce05c22
dulcetiseduce1 <mariusdulce@tuta.io>
2022-05-24 15:23:15 +0200

    snack3

--- snack1/js/main.js
+++ snack1/js/main.js
@@ -4,5 +4,5 @@ const numero = parseInt(prompt("Inserisci un numero"))
 if(numero%2 === 0 ){
     alert(`il numero ${numero} è pari`)
 }
-else
-    alert(`il numero ${numero} è dispari il numero successivo è ${numero + 1}`)
+else{
+    alert(`il numero ${numero} è dispari il numero successivo è ${numero + 1}`)}
--- snack3/js/main.js
+++ snack3/js/main.js
@@ -4,7 +4,13 @@ let somma = 0
 for(let i = 0; i<10; i++)
 {
     const ask = prompt("inserisci un numero")
-    listaNumeri.push(parseInt(ask))
-    somma = somma + parseInt(ask) 
+    if(ask%2 === 0){
+        console.log("pari" + ask)
+    }
+    else{
+        console.log("dispari" + ask)
+        somma = somma + parseInt(ask)
+    }
+         
 }
 console.log(somma)
